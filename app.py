6377f3e1da2d00da7939a0eb098d3289c4358979
andrew adel <68513548+andrew-adel-labib@users.noreply.github.com>
2025-08-16 21:03:37 +0300

    Andrew Adel

---
+++ app.py
@@ -0,0 +1,67 @@
+import os
+import time
+import streamlit as st
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+from langchain_openai import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import OllamaEmbeddings
+
+load_dotenv()
+
+# os.environ["OPEN_API_KEY"] = os.getenv("OPEN_API_KEY")
+os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
+groq_api_key = os.getenv("GROQ_API_KEY")
+
+llm = ChatGroq(groq_api_key=groq_api_key, model_name="Llama3-8b-8192")
+
+prompt = ChatPromptTemplate.from_template(
+    """
+    Answer the questions based on the provided context only.
+    Please provide the most accurate response based on the question.
+    <context>
+    {context}
+    <context>
+    Question: {input} 
+    """
+)
+
+st.title("RAG Document Q&A with Groq API and Llama3")
+
+def create_vector_embedding():
+    if "vectors" not in st.session_state:
+        # st.session_state.embeddings = OpenAIEmbeddings()
+        # st.session_state.embeddings = OllamaEmbeddings()
+        st.session_state.embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+        st.session_state.loader = PyPDFDirectoryLoader("research_papers")
+        st.session_state.docs = st.session_state.loader.load()
+        st.session_state.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+        st.session_state.final_documents = st.session_state.text_splitter.split_documents(st.session_state.docs[:50])
+        st.session_state.vectors = FAISS.from_documents(st.session_state.final_documents, st.session_state.embeddings)
+
+user_prompt = st.text_input("Enter your query from the research paper")
+
+if st.button("Document Embedding"):
+    create_vector_embedding()
+    st.write("Vector Database is ready.")
+
+if user_prompt:
+    document_chain = create_stuff_documents_chain(llm=llm, prompt=prompt)
+    retriever = st.session_state.vectors.as_retriever()
+    retrieval_chain = create_retrieval_chain(retriever, document_chain)
+
+    start = time.process_time()
+    response = retrieval_chain.invoke({"input": user_prompt})
+    print(f"Response Time is: {time.process_time() - start}")
+    st.write(response["answer"])
+
+    with st.expander("Document Similarity Search"):
+        for i, doc in enumerate(response["context"]):
+            st.write(doc.page_content)
+            st.write("---------------------------------------------------")
